1d1f68f82e0b9fe53de1adf2951f29bd5f278644
Julien Poulton <julien.poulton@gmail.com>
2017-12-21 11:47:39 +0100

    feat(viewer): Added new player colours

--- engine/core/src/main/resources/view/core/Drawer.js
+++ engine/core/src/main/resources/view/core/Drawer.js
@@ -25,6 +25,18 @@ export class Drawer {
   static getGameRatio() {
     return Drawer.WIDTH / Drawer.HEIGHT;
   }
+  static get playerColors() {
+    return [
+      '#ff0000', // solid red
+      '#22a1e4', // curious blue
+      '#ff8f16', // west side orange
+      '#6ac371', // mantis green
+      '#9975e2', // medium purple
+      '#3ac5ca', // scooter blue
+      '#de6ddf', // lavender pink      
+      '#ff1d5c'  // radical red
+    ];      
+  }
 
   instantiateModules() {
     this.modules = {};
