136ca2acd2ac48956f384eaf0214af8009ac6087
Julien Poulton <julien.poulton@gmail.com>
2018-02-16 16:04:24 +0100

    fix(entities, shapes): use pixel measurements

--- engine/core/src/main/resources/view/core/Drawer.js
+++ engine/core/src/main/resources/view/core/Drawer.js
@@ -363,7 +363,8 @@ export class Drawer {
       var stage = new PIXI.Container();
       module.reinitScene(stage, {
         width: scope.canvasWidth,
-        height: scope.canvasHeight
+        height: scope.canvasHeight,
+        oversampling: this.oversampling
       });
       container.addChild(stage);
     }
