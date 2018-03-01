52b48b754a23f64973d42a420568fc58efed108b
Julien Poulton <julien.poulton@gmail.com>
2018-03-01 10:56:50 +0100

    feat(entities): include both curves: _ and Γ

--- engine/modules/entities/src/main/java/com/codingame/gameengine/module/entities/Curve.java
+++ engine/modules/entities/src/main/java/com/codingame/gameengine/module/entities/Curve.java
@@ -10,9 +10,14 @@ public enum Curve {
     LINEAR,
 
     /**
-     * Don't animate the transition.
+     * Don't animate the transition, wait for next commit for the new value to be used in the display.
      */
     NONE,
+    
+    /**
+     * Don't animate the transition, immediately use the new value to be used in the display.
+     */
+    IMMEDIATE,
 
     /**
      * Start changing slowly, accelerate, then slowly again.
