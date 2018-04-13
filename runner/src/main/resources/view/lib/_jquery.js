d4b72ac03e84683b39e7ea4f1ac2ddf2096b3eab
Julien Poulton <julien@codingame.com>
2018-04-13 14:06:23 +0200

    update player lib

---
+++ runner/src/main/resources/view/lib/_jquery.js
@@ -0,0 +1,8 @@
+Unstaged changes after reset:
+M	engine/core/src/main/java/com/codingame/gameengine/core/GameManager.java
+M	runner/src/main/resources/view/app.js
+M	runner/src/main/resources/view/lib/_jquery.js
+D	runner/src/main/resources/view/lib/jquery.js
+D	runner/src/main/resources/view/lib/pixi4.js
+M	runner/src/main/resources/view/lib/player.js
+M	runner/src/main/resources/view/test.html
