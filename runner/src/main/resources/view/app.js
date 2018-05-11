08b800a5dcdeced15160bd80239012ef2c593979
Thomas Foucault <thomas@codingame.com>
2018-05-11 14:54:24 +0200

    cleanup(sdk): reformat using js standard style

--- runner/src/main/resources/view/app.js
+++ runner/src/main/resources/view/app.js
@@ -1,249 +1,249 @@
-import * as config from '../config.js';
-import {Drawer} from '../core/Drawer.js';
-import {ErrorLog} from '../core/ErrorLog.js';
-import {demo} from '../demo.js';
-import Parser from './lib/Parser.js';
+import * as config from '../config.js'
+import {Drawer} from '../core/Drawer.js'
+import {ErrorLog} from '../core/ErrorLog.js'
+import {demo} from '../demo.js'
+import Parser from './lib/Parser.js'
 
-function PlayerCtrl($scope, $timeout, $interval, $translate, drawerFactory, gameManagerFactory, $localStorage) {
-  'ngInject';
-  const ctrl = this;
-  let player = null;
-  let lastWidth;
-  let currentFrame = null;
+function PlayerCtrl ($scope, $timeout, $interval, $translate, drawerFactory, gameManagerFactory, $localStorage) {
+  'ngInject'
+  const ctrl = this
+  let player = null
+  let lastWidth
+  let currentFrame = null
 
   let playerLoadedPromise = new Promise((resolve) => {
-    $scope.playerLoaded = function(playerApi) {
-      ctrl.playerApi = playerApi;
-      resolve(playerApi);
-    };
-  });
+    $scope.playerLoaded = function (playerApi) {
+      ctrl.playerApi = playerApi
+      resolve(playerApi)
+    }
+  })
 
   $scope.gameParams = $localStorage.$default({
     gameParams: {}
-  }).gameParams;
-  $scope.loadGame = loadGame;
-  $scope.selectReplay = selectReplay;
-  $scope.exportZip = exportZip;
-  $scope.reportItems = {};
-  $scope.closeReportPopup = closeReportPopup;
-  $scope.submitConfig = submitConfig;
+  }).gameParams
+  $scope.loadGame = loadGame
+  $scope.selectReplay = selectReplay
+  $scope.exportZip = exportZip
+  $scope.reportItems = {}
+  $scope.closeReportPopup = closeReportPopup
+  $scope.submitConfig = submitConfig
 
-  $interval(checkSize, 1000);
+  $interval(checkSize, 1000)
 
-  $scope.errors = "";
-  ErrorLog.listen(function(error) {
-    $scope.errors += error.message + '\n';
-  });
+  $scope.errors = ''
+  ErrorLog.listen(function (error) {
+    $scope.errors += error.message + '\n'
+  })
 
-  init();
+  init()
 
-  /////////////////
+  /// //////////////
 
-  function init() {
+  function init () {
     drawerFactory.createDrawer(Drawer).then(drawer => {
-      $scope.drawer = drawer;
+      $scope.drawer = drawer
       let data = fetchGame().then(data => {
-        ctrl.data = data;
+        ctrl.data = data
         if (!demo && !config.demo) {
-          loadGame();
+          loadGame()
         }
-      });
-    });
+      })
+    })
   }
 
-  function loadGame() {
+  function loadGame () {
     if ($scope.gameLoaded || !ctrl.data) {
-      return;
+      return
     }
-    $scope.gameLoaded = true;
-    ctrl.gameInfo = convertFrameFormat(ctrl.data);
-    $scope.agents = {...ctrl.data.agents};
+    $scope.gameLoaded = true
+    ctrl.gameInfo = convertFrameFormat(ctrl.data)
+    $scope.agents = {...ctrl.data.agents}
 
-    ctrl.gameManager = gameManagerFactory.createGameManagerFromGameInfo($scope.drawer, ctrl.gameInfo, true);
-    ctrl.gameManager.subscribe(onUpdate);
+    ctrl.gameManager = gameManagerFactory.createGameManagerFromGameInfo($scope.drawer, ctrl.gameInfo, true)
+    ctrl.gameManager.subscribe(onUpdate)
 
     return playerLoadedPromise.then(playerApi => {
-      playerApi.initReplay(ctrl.gameManager);
-      playerApi.initReplay(ctrl.gameManager);
-    });
+      playerApi.initReplay(ctrl.gameManager)
+      playerApi.initReplay(ctrl.gameManager)
+    })
   }
 
-  function onUpdate(frame, progress, playing, isSubFrame, isTurnBased, atEnd) {
+  function onUpdate (frame, progress, playing, isSubFrame, isTurnBased, atEnd) {
     if (ctrl.gameInfo.frames[frame].keyframe && frame !== currentFrame) {
       $timeout(() => {
-        currentFrame = frame;
-        onFrameChange(frame);
-      });
+        currentFrame = frame
+        onFrameChange(frame)
+      })
     }
   }
 
-  function onFrameChange(frame) {
-    let startFrame = frame;
+  function onFrameChange (frame) {
+    let startFrame = frame
     while (startFrame > 0 && !ctrl.gameInfo.frames[startFrame - 1].keyframe) {
-      startFrame--;
+      startFrame--
     }
 
     for (var i in ctrl.data.ids) {
-      $scope.agents[i].stdout = null;
-      $scope.referee = {};
+      $scope.agents[i].stdout = null
+      $scope.referee = {}
     }
 
     while (startFrame <= frame) {
       for (var i in ctrl.data.ids) {
-        const stdout = ctrl.data.outputs[i][startFrame];
+        const stdout = ctrl.data.outputs[i][startFrame]
         if (stdout) {
-          $scope.agents[i].stdout = stdout;
+          $scope.agents[i].stdout = stdout
         }
-        const stderr = ctrl.data.errors[i][startFrame];
+        const stderr = ctrl.data.errors[i][startFrame]
         if (stderr) {
-          $scope.agents[i].stderr = stderr;
+          $scope.agents[i].stderr = stderr
         }
       }
-      $scope.referee.stdout = $scope.referee.stdout || ctrl.data.outputs.referee[startFrame];
-      $scope.summary = convertNameTokens(ctrl.data.summaries[startFrame]);
-      startFrame++;
+      $scope.referee.stdout = $scope.referee.stdout || ctrl.data.outputs.referee[startFrame]
+      $scope.summary = convertNameTokens(ctrl.data.summaries[startFrame])
+      startFrame++
     }
   }
 
-  function convertNameTokens(value) {
-    return value && value.replace(/\$(\d)/g, 'Player $1');
+  function convertNameTokens (value) {
+    return value && value.replace(/\$(\d)/g, 'Player $1')
   }
-  
-  function convertFrameFormat(data) {
+
+  function convertFrameFormat (data) {
     const frames = data.views.map(v => {
-      let f = v.split('\n');
-      let header = f[0].split(' ');
+      let f = v.split('\n')
+      let header = f[0].split(' ')
 
-      return {view: v.replace(/^(KEY_FRAME)|(INTERMEDIATE_FRAME)/, ''), keyframe: header[0] === 'KEY_FRAME'};
-    });
+      return {view: v.replace(/^(KEY_FRAME)|(INTERMEDIATE_FRAME)/, ''), keyframe: header[0] === 'KEY_FRAME'}
+    })
     for (let i = 0; i < frames.length; i++) {
-      frames[i].gameSummary = data.summaries[i];
+      frames[i].gameSummary = data.summaries[i]
       for (var pi in data.ids) {
-        frames[i].stderr = frames[i].stderr || data.errors[pi][i];
-        frames[i].stdout = frames[i].stdout || data.outputs[pi][i];
+        frames[i].stderr = frames[i].stderr || data.errors[pi][i]
+        frames[i].stdout = frames[i].stdout || data.outputs[pi][i]
       }
-      frames[i].agentId = -1;
+      frames[i].agentId = -1
     }
-    const agents = data.agents.map(a => Object.assign(a, {avatarUrl: a.avatar}));
-    const tooltips = data.tooltips.map(JSON.stringify);
-    return {agents: agents, frames: frames, tooltips: tooltips};
+    const agents = data.agents.map(a => Object.assign(a, {avatarUrl: a.avatar}))
+    const tooltips = data.tooltips.map(JSON.stringify)
+    return {agents: agents, frames: frames, tooltips: tooltips}
   }
 
-  function checkSize() {
+  function checkSize () {
     if (!player) {
-      player = $('#cg-player').find('.player');
+      player = $('#cg-player').find('.player')
     }
-    const newWidth = player.width();
+    const newWidth = player.width()
     if (newWidth !== lastWidth) {
-      lastWidth = newWidth;
+      lastWidth = newWidth
       if (ctrl.playerApi) {
-        ctrl.playerApi.resize();
+        ctrl.playerApi.resize()
       }
     }
   }
 
-  function fetchGame() {
+  function fetchGame () {
     return new Promise((resolve, reject) => {
-      let xhr = new XMLHttpRequest();
-      xhr.onload = function() {
-        let result = null;
+      let xhr = new XMLHttpRequest()
+      xhr.onload = function () {
+        let result = null
         try {
-          const json = JSON.parse(this.responseText);
-          json.agents.forEach(agent => agent.color = Drawer.playerColors[agent.index]);
+          const json = JSON.parse(this.responseText)
+          json.agents.forEach(agent => agent.color = Drawer.playerColors[agent.index])
           result = json
         } catch (e) {
-          console.error(e);
-          reject(e);
+          console.error(e)
+          reject(e)
         }
-        resolve(result);
-      };
-      xhr.open('GET', 'game.json', true);
-      xhr.send();
-    });
+        resolve(result)
+      }
+      xhr.open('GET', 'game.json', true)
+      xhr.send()
+    })
   }
-  $scope.selectProgress = 'inactive';
-  async function selectReplay() {
-    $scope.selectProgress = 'saving';
-    const response = await fetch('/services/save-replay');
-    $scope.selectProgress = 'complete';
-  }
-
-  function closeReportPopup() {
-    $scope.showExport = false;
+  $scope.selectProgress = 'inactive'
+  async function selectReplay () {
+    $scope.selectProgress = 'saving'
+    const response = await fetch('/services/save-replay')
+    $scope.selectProgress = 'complete'
   }
 
-  function closeConfigForm() {
-    $scope.showConfigForm = false;
+  function closeReportPopup () {
+    $scope.showExport = false
   }
 
-  $scope.showExport = false;
-  $scope.showConfigForm = false;
-  async function exportZip() {
-    const data = await fetch('/services/export');
+  function closeConfigForm () {
+    $scope.showConfigForm = false
+  }
+
+  $scope.showExport = false
+  $scope.showConfigForm = false
+  async function exportZip () {
+    const data = await fetch('/services/export')
     if (data.status === 422) {
-      const text = await data.text();
-      $scope.formStatement = text;
-      $scope.showConfigForm = true;
+      const text = await data.text()
+      $scope.formStatement = text
+      $scope.showConfigForm = true
     } else {
-      const exportResponseString = await data.text();
-      var exportResponse = JSON.parse(exportResponseString);
+      const exportResponseString = await data.text()
+      var exportResponse = JSON.parse(exportResponseString)
 
-      var stubParser = new Parser();
+      var stubParser = new Parser()
       for (var stub in exportResponse.stubs) {
         try {
-          stubParser.parse(exportResponse.stubs[stub], 0);
+          stubParser.parse(exportResponse.stubs[stub], 0)
         } catch (e) {
           exportResponse.reportItems.push({
-            "type": "WARNING",
-            "message": stub + " Error in stub.txt",
-            "details": { "name": e.name, "params": e.params }
-          });
+            'type': 'WARNING',
+            'message': stub + ' Error in stub.txt',
+            'details': { 'name': e.name, 'params': e.params }
+          })
         }
       }
 
-      if (exportResponse.exportStatus === "SUCCESS") {
+      if (exportResponse.exportStatus === 'SUCCESS') {
         exportResponse.reportItems.push({
-          "type": "SUCCESS",
-          "message": "Export success."
-        });
-        var url = window.URL.createObjectURL(base64ToBlob(exportResponse.data));
-        var a = document.createElement('a');
-        a.href = url;
-        a.download = "export.zip";
-        a.click();
+          'type': 'SUCCESS',
+          'message': 'Export success.'
+        })
+        var url = window.URL.createObjectURL(base64ToBlob(exportResponse.data))
+        var a = document.createElement('a')
+        a.href = url
+        a.download = 'export.zip'
+        a.click()
       } else {
         exportResponse.reportItems.push({
-          "type": "FAIL",
-          "message": "Export fail."
-        });
+          'type': 'FAIL',
+          'message': 'Export fail.'
+        })
       }
-      $scope.reportItems = exportResponse.reportItems;
-      $scope.showExport = true;
+      $scope.reportItems = exportResponse.reportItems
+      $scope.showExport = true
     }
   }
 
-  function base64ToBlob(base64) {
-    var binary_string = window.atob(base64);
-    var len = binary_string.length;
-    var bytes = new Uint8Array(len);
+  function base64ToBlob (base64) {
+    var binary_string = window.atob(base64)
+    var len = binary_string.length
+    var bytes = new Uint8Array(len)
     for (var i = 0; i < len; i++) {
-      bytes[i] = binary_string.charCodeAt(i);
+      bytes[i] = binary_string.charCodeAt(i)
     }
-    return new Blob([bytes]);
+    return new Blob([bytes])
   }
 
-  async function submitConfig(valid, config){
-    if(!valid){
-      return;
+  async function submitConfig (valid, config) {
+    if (!valid) {
+      return
     }
-    await fetch('/services/init-config', 
-    { 
-      body: JSON.stringify(config), 
-      method: 'POST'
-    });
-    closeConfigForm();
-    exportZip();
+    await fetch('/services/init-config',
+      {
+        body: JSON.stringify(config),
+        method: 'POST'
+      })
+    closeConfigForm()
+    exportZip()
   }
 }
 
-angular.module('player').controller('PlayerCtrl', PlayerCtrl);
+angular.module('player').controller('PlayerCtrl', PlayerCtrl)
