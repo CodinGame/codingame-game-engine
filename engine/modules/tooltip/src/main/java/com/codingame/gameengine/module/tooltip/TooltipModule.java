a1be72f6efc6f93b636d7143464bd2603894a1ba
Unknown <0mindstorming@gmail.com>
2019-01-22 14:57:11 +0100

    feat(tooltip): adding the tooltipmodule

---
+++ engine/modules/tooltip/src/main/java/com/codingame/gameengine/module/tooltip/TooltipModule.java
@@ -0,0 +1,96 @@
+package com.codingame.gameengine.module.tooltip;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.codingame.gameengine.core.AbstractPlayer;
+import com.codingame.gameengine.core.GameManager;
+import com.codingame.gameengine.core.Module;
+import com.codingame.gameengine.module.entities.Entity;
+import com.codingame.gameengine.module.entities.GraphicEntityModule;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+/**
+ * The TooltipModule takes care of displaying tooltips under the mouse cursor when an element has a linked tooltip text.
+ * 
+ */
+@Singleton
+public class TooltipModule implements Module {
+
+    GameManager<AbstractPlayer> gameManager;
+    @Inject GraphicEntityModule entityModule;
+    Map<Integer, String> registered, newRegistration;
+
+    @Inject
+    TooltipModule(GameManager<AbstractPlayer> gameManager) {
+        this.gameManager = gameManager;
+        gameManager.registerModule(this);
+        registered = new HashMap<>();
+        newRegistration = new HashMap<>();
+    }
+
+    @Override
+    public void onGameInit() {
+        sendFrameData();
+    }
+
+    @Override
+    public void onAfterGameTurn() {
+        sendFrameData();
+    }
+
+    @Override
+    public void onAfterOnEnd() {
+        sendFrameData();
+    }
+
+    private void sendFrameData() {
+        Object[] data = { newRegistration };
+        gameManager.setViewData("tooltips", data);
+
+        newRegistration.clear();
+    }
+
+    boolean stringEquals(String a, String b) {
+        if (a == b) {
+            return true;
+        } else if (a != null && a.equals(b)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Sets a tooltip text linked to an entity
+     * 
+     * @param entity
+     * @param text is the tooltip text that will be displayed when hovering over the entity
+     */
+    public void setTooltipText(Entity<?> entity, String text) {
+        int id = entity.getId();
+        if (!stringEquals(text, registered.get(id))) {
+            newRegistration.put(id, text);
+            registered.put(id, text);
+        }
+    }
+
+    /**
+     * 
+     * @param entity
+     * @return the tooltip text liked to the entity
+     */
+    public String getTooltipText(Entity<?> entity) {
+        return registered.get(entity.getId());
+    }
+
+    /**
+     * Removes the tooltip text linked to the entity
+     * @param entity
+     */
+    public void removeTooltipText(Entity<?> entity) {
+        newRegistration.put(entity.getId(), null);
+        registered.remove(entity.getId());
+    }
+}
