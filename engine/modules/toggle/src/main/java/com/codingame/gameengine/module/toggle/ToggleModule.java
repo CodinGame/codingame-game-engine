ed634cc8714dcc6a91bc2005d5f9cd7f46a6c440
Unknown <0mindstorming@gmail.com>
2019-01-23 15:20:59 +0100

    feat(sdk): adding toggle module

---
+++ engine/modules/toggle/src/main/java/com/codingame/gameengine/module/toggle/ToggleModule.java
@@ -0,0 +1,95 @@
+package com.codingame.gameengine.module.toggle;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.codingame.gameengine.core.AbstractPlayer;
+import com.codingame.gameengine.core.GameManager;
+import com.codingame.gameengine.core.Module;
+import com.codingame.gameengine.module.entities.Entity;
+import com.codingame.gameengine.module.entities.GraphicEntityModule;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+/**
+ * @author Jean Porée
+ * 
+ *         The toggle module allows to display or hide elements from the graphicEntityModule using the cg-player option toggles.
+ * 
+ */
+@Singleton
+public class ToggleModule implements Module {
+
+    GameManager<AbstractPlayer> gameManager;
+    @Inject GraphicEntityModule entityModule;
+    Map<Integer, Toggle> registered, newRegistration;
+
+    class Toggle {
+        public String name;
+        public Boolean state = true;
+
+        public Toggle(String name, Boolean state) {
+            this.name = name;
+            this.state = state;
+        }
+
+        public Boolean equals(Toggle other) {
+            return other != null && this.state == other.state && stringEquals(this.name, other.name);
+        }
+
+        boolean stringEquals(String a, String b) {
+            if (a == b) {
+                return true;
+            } else if (a != null && a.equals(b)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    @Inject
+    ToggleModule(GameManager<AbstractPlayer> gameManager) {
+        this.gameManager = gameManager;
+        gameManager.registerModule(this);
+        registered = new HashMap<>();
+        newRegistration = new HashMap<>();
+    }
+
+    @Override
+    public void onGameInit() {
+        sendFrameData();
+    }
+
+    @Override
+    public void onAfterGameTurn() {
+        sendFrameData();
+    }
+
+    @Override
+    public void onAfterOnEnd() {
+        sendFrameData();
+    }
+
+    private void sendFrameData() {
+        Object[] data = { newRegistration };
+        gameManager.setViewData("toggles", data);
+
+        newRegistration.clear();
+    }
+    /**
+     * Will display the entity only when the toggle state matches the state you set
+     * 
+     * @param entity which will be displayed
+     * @param toggle the name of the toggle you want to use
+     * @param state the state of the toggle where the entity will be displayed at
+     */
+    public void displayOnToggleState(Entity<?> entity, String toggle, Boolean state) {
+        int id = entity.getId();
+        Toggle associatedToggle = new Toggle(toggle, state);
+        if (!associatedToggle.equals(registered.get(id))) {
+            newRegistration.put(id, associatedToggle);
+            registered.put(id, associatedToggle);
+        }
+    }
+}
---
+++ engine/modules/toggle/src/main/resources/view/toggle-module/ToggleModule.js
@@ -0,0 +1,55 @@
+import { api as entityModule } from '../../entity-module/GraphicEntityModule.js'
+import { toggles } from '../../config.js'
+import {ErrorLog} from '../core/ErrorLog.js'
+import {MissingToggleError} from './errors/MissingToggleError.js'
+
+export class ToggleModule {
+  constructor (assets) {
+    this.previousFrame = {}
+    this.missingToggles = {}
+    ToggleModule.refreshContent = () => {
+      if (!this.currentFrame) {
+        return
+      }
+      for (const registeredEntity in this.currentFrame.registered) {
+        const entity = entityModule.entities.get(parseInt(registeredEntity))
+        const toggleInfo = this.currentFrame.registered[registeredEntity]
+        const toggleState = toggles[toggleInfo.name]
+
+        if (toggleState === undefined && !this.missingToggles[toggleInfo.name]) {
+          ErrorLog.push(new MissingToggleError(toggleInfo.name))
+          this.missingToggles[toggleInfo.name] = true
+        }
+        entity.setHidden(
+          toggleState !== toggleInfo.state
+        )
+      }
+    }
+  }
+  static refreshContent () {}
+
+  static get name () {
+    return 'toggles'
+  }
+
+  updateScene (previousData, currentData, progress) {
+    this.currentFrame = currentData
+    this.currentProgress = progress
+    ToggleModule.refreshContent()
+  }
+
+  handleFrameData (frameInfo, data) {
+    if (!data) {
+      return
+    }
+    const newRegistration = data[0]
+    const registered = { ...this.previousFrame.registered, ...newRegistration }
+    const frame = { registered, number: frameInfo.number }
+    this.previousFrame = frame
+    return frame
+  }
+
+  reinitScene (container, canvasData) {
+    ToggleModule.refreshContent()
+  }
+}
