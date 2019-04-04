dc4b4c8b294a8458a7831ee7dd12c64b0383ae1d
toerau marot <toerau.marot@codingame.com>
2019-04-04 09:48:48 +0200

    refactor(SDK): DuplicateToggleValue error message

--- engine/modules/toggle/src/main/resources/view/toggle-module/errors/DuplicateToggleValueError.js
+++ engine/modules/toggle/src/main/resources/view/toggle-module/errors/DuplicateToggleValueError.js
@@ -1,6 +1,6 @@
 export class DuplicateToggleValueError extends Error {
   constructor (toggle, dup, cause) {
-    super('(' + dup.keys.toString() + ') have same value \'' + dup.value + '\' in toggle "' + toggle + '". Avoid those duplicates in your config.js.')
+    super('(' + dup.keys.toString() + ') have the same value \'' + dup.value + '\' in toggle "' + toggle + '". Avoid those duplicates in your config.js.')
     this.toggle = toggle
     this.cause = cause
     this.name = 'DuplicateToggleValueError'
